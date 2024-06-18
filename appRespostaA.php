0a9d6863d8ad25907786c2d15bb0564dd2d31d2d
Rodolfo Gama <rodolfo.gama2006@gmail.com>
2024-06-17 21:01:38 -0300

    Update tentativa 1 de cadastro alternativa

--- appRespostaA.php
+++ appRespostaA.php
@@ -1,14 +1,53 @@
 <?php 
+include("conexao.php");
+include("appCadastroPergunta.php");
+
 $alternativa1 = $_POST['a'];
 $alternativa2 = $_POST['b'];
 $alternativa3 = $_POST['c'];
 $alternativa4 = $_POST['d'];
 $correta = $_POST['correta'];
 
-echo "Alternativa A: " . $alternativa1 . "<br>";
-echo "Alternativa B: " . $alternativa2 . "<br>";
-echo "Alternativa C: " . $alternativa3 . "<br>";
-echo "Alternativa D: " . $alternativa4 . "<br>";
-echo "Alternativa correta: " . $correta;
+$sql = "SELECT idPergunta FROM `pergunta` WHERE `enunciado` = '$inputEnunciado' AND `idmateria` = $inputMateria AND `idtipo` = $inputTipoPergunta;";
+$result = mysqli_query($conexao, $sql);
+
+
+// echo "Alternativa A: " . $alternativa1 . "<br>";
+// echo "Alternativa B: " . $alternativa2 . "<br>";
+// echo "Alternativa C: " . $alternativa3 . "<br>";
+// echo "Alternativa D: " . $alternativa4 . "<br>";
+// echo "Alternativa correta: " . $correta;
+
+if ($correta = 1) {
+    $sql = "INSERT INTO `resposta` (`idResposta`, `descricao`, `idalternativa`, `idperg`) VALUES (NULL, '$alternativa1', '1', '$result');";
+    $result = mysqli_query($conexao, $sql);
+} else {
+    $sql = "INSERT INTO `resposta` (`idResposta`, `descricao`, `idalternativa`, `idperg`) VALUES (NULL, '$alternativa1', '2', '$result');";
+    $result = mysqli_query($conexao, $sql);
+}
+
+if ($correta = 2) {
+    $sql = "INSERT INTO `resposta` (`idResposta`, `descricao`, `idalternativa`, `idperg`) VALUES (NULL, '$alternativa2', '1', '$result');";
+    $result = mysqli_query($conexao, $sql);
+} else {
+    $sql = "INSERT INTO `resposta` (`idResposta`, `descricao`, `idalternativa`, `idperg`) VALUES (NULL, '$alternativa2', '2', '$result');";
+    $result = mysqli_query($conexao, $sql);
+}
+
+if ($correta = 3) {
+    $sql = "INSERT INTO `resposta` (`idResposta`, `descricao`, `idalternativa`, `idperg`) VALUES (NULL, '$alternativa3', '1', '$result');";
+    $result = mysqli_query($conexao, $sql);
+} else {
+    $sql = "INSERT INTO `resposta` (`idResposta`, `descricao`, `idalternativa`, `idperg`) VALUES (NULL, '$alternativa3', '2', '$result');";
+    $result = mysqli_query($conexao, $sql);
+}
+
+if ($correta = 4) {
+    $sql = "INSERT INTO `resposta` (`idResposta`, `descricao`, `idalternativa`, `idperg`) VALUES (NULL, '$alternativa4', '1', '$result');";
+    $result = mysqli_query($conexao, $sql);
+} else {
+    $sql = "INSERT INTO `resposta` (`idResposta`, `descricao`, `idalternativa`, `idperg`) VALUES (NULL, '$alternativa4', '2', '$result');";
+    $result = mysqli_query($conexao, $sql);
+}
 
 ?>
